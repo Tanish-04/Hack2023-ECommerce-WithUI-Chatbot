e2689f95fe6350165f47b6663a11373ba55ed214
oldbright22 <125103467+oldbright22@users.noreply.github.com>
2023-10-30 12:10:30 +0000

    e

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,7 +1,7 @@
 
 import os
 from gtts import gTTS
-from playsound import playsound
+import pygame
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
@@ -158,6 +158,19 @@ class MaverickChatbot:
             # Save the converted audio to a file
             filename = "temp_audio.mp3"
             tts.save(filename)
+
+            #Initialize pygame mixer
+            pygame.mixer.init()
+                
+            # Load and play the saved audio file
+            pygame.mixer.music.load(filename)
+            pygame.mixer.music.play()
+
+            # Keep the program running until the audio finishes playing
+            while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
+
+
 
             # Play the saved file
             #playsound(filename)
